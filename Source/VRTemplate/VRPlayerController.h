d223e55a7c59d2927da5fe90a4520ae875882aeb
Firestack <firestacks@CSGP.EDU>
2016-04-26 15:34:05 -0600

    Init

---
+++ Source/VRTemplate/VRPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "VRPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRTEMPLATE_API AVRPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
