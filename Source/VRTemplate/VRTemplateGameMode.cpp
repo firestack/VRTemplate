942dbd1f1aed2926268ecb28b63bba5cd3e5958f
Firestack <me@stackfire.io>
2016-04-26 23:45:38 -0600

    Update and add functions to game mode to allow for spawning at the base of the PlayerStart as well as spawning a VR player based on if VR is enabled

--- Source/VRTemplate/VRTemplateGameMode.cpp
+++ Source/VRTemplate/VRTemplateGameMode.cpp
@@ -3,6 +3,45 @@
 #include "VRTemplate.h"
 #include "VRTemplateGameMode.h"
 
+#include "VRPawn.h"
 
 
+AVRTemplateGameMode::AVRTemplateGameMode() {
+	DefaultVRPawnClass = AVRPawn::StaticClass();
+}
+
+APawn* AVRTemplateGameMode::SpawnDefaultPawnFor_Implementation(AController* NewPlayer, AActor* StartSpot)
+{
+	// Select Correct Spawn Position based on VR mode
+	FVector StartLocation = StartSpot->GetActorLocation();
+	if (ShouldUseVR()) {
+
+		StartLocation.Z -= StartSpot->GetRootComponent()->Bounds.BoxExtent.Z;
+	}
+
+	// don't allow pawn to be spawned with any pitch or roll
+	FRotator StartRotation(ForceInit);
+	StartRotation.Yaw = StartSpot->GetActorRotation().Yaw;
+
+	FActorSpawnParameters SpawnInfo;
+	SpawnInfo.Instigator = Instigator;
+	SpawnInfo.ObjectFlags |= RF_Transient;	// We never want to save default player pawns into a map
+	UClass* PawnClass = GetDefaultPawnClassForController(NewPlayer);
+	APawn* ResultPawn = GetWorld()->SpawnActor<APawn>(PawnClass, StartLocation, StartRotation, SpawnInfo);
+	if (ResultPawn == NULL)
+	{
+		//UE_LOG(LogGameMode, Warning, TEXT("Couldn't spawn Pawn of type %s at %s"), *GetNameSafe(PawnClass), *StartSpot->GetName()); 
+	}
+	return ResultPawn;
+}
+
+bool AVRTemplateGameMode::ShouldUseVR_Implementation()
+{
+	return UHeadMountedDisplayFunctionLibrary::IsHeadMountedDisplayEnabled() && UseVR;
+}
+
+UClass * AVRTemplateGameMode::GetDefaultPawnClassForController_Implementation(AController * InController)
+{
+	return (ShouldUseVR() ? DefaultVRPawnClass : DefaultPawnClass);
+}
 
--- Source/VRTemplate/VRTemplateGameMode.h
+++ Source/VRTemplate/VRTemplateGameMode.h
@@ -13,7 +13,32 @@ class VRTEMPLATE_API AVRTemplateGameMode : public AGameMode
 {
 	GENERATED_BODY()
 	
-	
-	
-	
+
+
+public:
+	AVRTemplateGameMode();
+	/**
+	* Custom VR Implementation
+	* @param	NewPlayer - Controller for whom this pawn is spawned
+	* @param	StartSpot - PlayerStart at which to spawn pawn
+	* @return	a pawn of the default pawn class
+	*/
+	UFUNCTION(BlueprintNativeEvent, Category = "Game")
+	APawn* SpawnDefaultPawnFor(AController* NewPlayer, class AActor* StartSpot);
+	virtual APawn* SpawnDefaultPawnFor_Implementation(AController* NewPlayer, class AActor* StartSpot) override;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintPure, Category = "Game")
+	bool ShouldUseVR();
+	virtual bool ShouldUseVR_Implementation();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Game")
+	UClass* GetDefaultPawnClassForController(AController* InController);
+	virtual UClass* GetDefaultPawnClassForController_Implementation(AController* InController);
+
+
+	UPROPERTY(EditAnywhere, noclear, BlueprintReadOnly, Category = Classes)
+		TSubclassOf<class APawn> DefaultVRPawnClass;
+
+	UPROPERTY(EditAnywhere, noclear, BlueprintReadOnly, Category = Classes)
+		bool UseVR;
 };
